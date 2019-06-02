305c73f8850d991cf4b50a9cce1ba6a7cb2208a5
t3k3 <teke629@gmail.com>
2019-06-02 15:57:29 +0300

    add bootstrap

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace CourseApp
 {
@@ -26,6 +28,20 @@ namespace CourseApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            //metod ile default olarak wwwroot klasörü dışarıya açılır.
+            //wwwroot klasöründe img/ css /js gibi dosyalar bulunabilir
+            app.UseStaticFiles();
+
+            /*Spesifik dosya dışarıya açmak için metoda parametre veririz. Directory.GetCurrentDirectory bize ana dizini 
+            verir. İkinci parametre klasörü gösteriyor. Sonuç olarak "bulunduğumuz_klasor/node_modules" döndürecektir.
+            Devamında RequestPath = "/modules" yazarak node_modules ismini "modules" olarak değiştiriyoruz. */
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),
+                RequestPath = "/modules"
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
